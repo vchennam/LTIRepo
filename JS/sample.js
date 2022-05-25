3f817bbe48b435cd3d68ef7d7c167429f2df5d48
vijaychennam <vijaychennam@gmail.com>
2022-05-25 23:32:51 +0530

    Initial commit

---
+++ JS/sample.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1 id="myH"></h1>
+<p id="myP"></p>
+
+<script>
+// Change heading:
+document.getElementById("myH").innerHTML = "JavaScript File";
+// Change paragraph:
+document.getElementById("myP").innerHTML = "LTI client evaluation";
+</script>
+
+</body>
+</html>
